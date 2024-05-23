18cda44106835ce4cbdb38f0b6be623b8065ccde
Daniel Felipe Vargas <d.vargasu@uniandes.edu.co>
2024-05-23 07:18:40 -0500

    comanod de nest

--- src/main.ts
+++ src/main.ts
@@ -15,3 +15,26 @@ async function bootstrap() {
   await app.listen(3000);
 }
 bootstrap();
+
+
+// TLDR CLI COMMANDS FOR NESTJS
+
+// 1. Crear un nuevo mod¿dulo
+
+// nest g module NAME
+
+// 2. Crear un nuevo modulo
+
+// nest g cl NAME/NAME.entity --no-spec
+
+// 3. Crear un nuevo servicio
+
+// nest g s NAME
+
+// 4. Crear un nuevo controlador
+
+// nest g co NAME --no-spec
+
+// 5. Crear un nuevo DTO
+
+// nest g cl NAME/NAME.dto --no-spec
